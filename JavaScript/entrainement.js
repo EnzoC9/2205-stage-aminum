46b1f8f432e29129683592b382553a92a3739114
EnzoC9 <enzo.carle95@gmail.com>
2022-05-16 17:14:58 +0200

    mise a jour

---
+++ JavaScript/entrainement.js
@@ -0,0 +1,27 @@
+
+
+let intergerCalculation = 1 + 2;
+let weirdCalculation = 0.1 + 0.2;
+console.log;
+
+let userIsSignedIn = true;
+let userIsAdmin = false:
+
+let firstName = "Will";
+let lastname = 'Alexander';
+let WholeName = firstName + "" + lastname;
+
+
+const myName = `Alexandre`;
+const salutation = `Bienvenue sur mon site ${myName}!`;
+
+let episodeTitle = ("lemechant");
+let episodeDuration = ("30");
+let hasBeenWatched = false
+
+let episode = {
+title: "the war",
+duration: 45,
+hasBennWatched: false
+
+};
