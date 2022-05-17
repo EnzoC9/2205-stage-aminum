784e7bc6d923546ce45fb40fca0c6190f8b536f9
EnzoC9 <enzo.carle95@gmail.com>
2022-05-17 17:39:23 +0200

    JS

---
+++ todo/todo.js
@@ -0,0 +1,7 @@
+let paragraphe = document.getElementById("paragraphe");
+let pseudo = prompt(" Entrez votre prenom");
+paragraphe.innerHTML ="Bonjour " + pseudo; 
+let ecrito = document.getElementById("ecrito");
+ecrito.innerHTML = "6.Terminer mes blocs JS: ";
+
+
