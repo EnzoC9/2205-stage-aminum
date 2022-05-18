626587ee05c82fb4788da2f52315d09dc1f9e55b
EnzoC9 <enzo.carle95@gmail.com>
2022-05-18 17:18:34 +0200

    mise a jour

--- todo/todo.js
+++ todo/todo.js
@@ -1,7 +1,24 @@
 let paragraphe = document.getElementById("paragraphe");
 let pseudo = prompt(" Entrez votre prenom");
-paragraphe.innerHTML ="Bonjour " + pseudo; 
+paragraphe.innerHTML = "Bonjour " + pseudo;
 let ecrito = document.getElementById("ecrito");
 ecrito.innerHTML = "6.Terminer mes blocs JS: ";
+
+let calculation = 4 + 7.5;
+console.log("le resultat est : ",calculation);
+//bollean//
+let nom = ("Enzo");
+let beau = true;
+let pasbeau = false;
+console.log('mon nom est : ',nom);
+console.log('il fait beau : ',beau)
+
+let fruit = ["mangue",
+    "pomme",
+    "banane",
+    "cerise",
+    "kiwi",
+    "poire"];
+console.log(fruit);
 
 
