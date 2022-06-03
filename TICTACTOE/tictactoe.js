30488434f1be81727e6797f47b053785ec865b83
EnzoC9 <enzo.carle95@gmail.com>
2022-06-03 11:41:08 +0200

    a jour

--- TICTACTOE/tictactoe.js
+++ TICTACTOE/tictactoe.js
@@ -1,21 +1,24 @@
 let all = document.querySelector("td");
 let jeuMorpion = [" 1", "2", "3", "4 ", "", "", "", "", "",];
 let joueurX = "X";
-let victioire = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+let joueurO = "O";
 
-document.querySelectorAll(".case").forEach(cellules =>
-    cellules.addEventListener("click", boutonclic));
 
-function boutonclic(){
-    console.log(this);
+
+ document.querySelectorAll(".case").forEach(cellule =>
+     cellule.addEventListener("click", boutonclic));
+
+ function boutonclic(){
+     console.log(this);
     
-};
+ };
+ 
+ let init = document.getElementById("btnInit");
+
+ init.addEventListener("click", clicBoutonInit);
+function clicBoutonInit() {
+    afficheJeu(jeuMorpion);
+}
+
+
+ 
